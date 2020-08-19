c05c7c218d34018282389c31e9fe43d00d2f9bbd
Margo <margo.bozhenova@gmail.com>
2020-08-19 16:44:13 +0300

    Add initial files and configs

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+  browserSync = require('browser-sync'),
+  autoprefixer = require('gulp-autoprefixer'),
+  rename = require('gulp-rename'),
+  minify = require('gulp-csso');
+
+gulp.task('css', function () {
+  return (
+    gulp
+      .src('./css/*.css')
+      .pipe(autoprefixer(['last 4 versions'], { cascade: true }))
+      .pipe(gulp.dest('./css'))
+      // .pipe(minify())
+      // .pipe(gulp.dest('./css'))
+      // .pipe(rename({ suffix: '.min' }))
+      // .pipe(gulp.dest('./css'))
+      .pipe(browserSync.reload({ stream: true }))
+  );
+});
+
+gulp.task('html', function () {
+  return gulp.src('./*.html').pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('js', function () {
+  return gulp.src('./*.js').pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: { baseDir: './' }
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./css/*.css', gulp.parallel('css'));
+  gulp.watch('./*.html', gulp.parallel('html'));
+  gulp.watch('./*.js', gulp.parallel('js'));
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
